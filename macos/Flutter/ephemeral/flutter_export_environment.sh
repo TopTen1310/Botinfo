4ca89c069f939c40c97035a7affc5934695b2db0
cizodevahm <93611338+cizodevahm@users.noreply.github.com>
2023-03-01 13:07:29 +0530

    1.1 Stable release with voice command

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sixty13/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sixty13/Botinfo"
+export "FLUTTER_ROOT=/Users/yogesh6013/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yogesh6013/Botinfov2v"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
